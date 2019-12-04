f7c2a9a5481ce4653e6d01c41e01ad3259f7ec18
Ulther <bnaylr@yahoo.co.uk>
2019-12-04 14:02:14 +0000

    The Riddler strikes againgit add .

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,60 +1,54 @@
 import React from "react";
 import placeholderImage from "./placeholderImage.jpg";
 
-const Profile = ({heroData}) => {
+const Profile = ({ heroData }) => {
+  return (
+    <ul className="heroList">
+      {heroData.map(hero => {
+        let heroImage;
+        if (hero) {
+          heroImage = hero.image.url;
+        } else {
+          heroImage = placeholderImage;
+        }
+        return (
+          <li key={hero.id} className="heroProfileListItem">
+            <img className="profileImage" src={heroImage} alt="Image"></img>
+            <ul className="heroDetailslist">
+              <label className="detailsLabel" htmlFor="heroName">
+                Known as:
+              </label>
+              <li className="detailsText" id="heroName">
+                {hero.name}
+              </li>
 
-    return (
-        <ul className="heroList"> 
-          { heroData.map(hero => {
-            let heroImage;
-            if (hero) {
-                heroImage = hero.image.url;
-            } else {
-                heroImage = placeholderImage;
-            }
-              return(
+              <label className="detailsLabel" htmlFor="heroRealName">
+                Full-name:
+              </label>
+              <li className="detailsText" id="heroRealName">
+                {hero.biography["full-name"]}
+              </li>
 
-                
-                <li key = {hero.id} className="heroProfileListItem">
-                    <img className="profileImage"
-                        src={
-                            heroImage
-                        }
-                        alt="Image"></img>
-                    <ul className="heroDetailslist">
-                        <label htmlFor="heroName">Known as:
-                        </label>
-                        <li id="heroName">
-                            {
-                            hero.name
-                        }</li>
+              <label className="detailsLabel" htmlFor="heroAlignment">
+                Alignment:
+              </label>
+              <li className="detailsText" id="heroAlignment">
+                {hero.biography.alignment[0].toUpperCase() +
+                  hero.biography.alignment.slice(1)}
+              </li>
 
-                        <label htmlFor="heroRealName">Full-name:
-                        </label>
-                        <li id="heroRealName">
-                            {
-                            hero.biography["full-name"]
-                        }</li>
-
-                        <label htmlFor="heroAlignment">Alignment:
-                        </label>
-                        <li id="heroAlignment">
-                            {
-                            hero.biography.alignment[0].toUpperCase() + hero.biography.alignment.slice(1)
-                        }</li>
-
-                        <label htmlFor="heroAffiliation">Affiliations:
-                        </label>
-                        <li id="heroAffiliation">
-                            {
-                            hero.connections["group-affiliation"]
-                        }</li>
-                    </ul>
-                </li>
-              )
-              })
-        } </ul>
-    );
+              <label className="detailsLabel" htmlFor="heroAffiliation">
+                Affiliations:
+              </label>
+              <li className="detailsText" id="heroAffiliation">
+                {hero.connections["group-affiliation"]}
+              </li>
+            </ul>
+          </li>
+        );
+      })}{" "}
+    </ul>
+  );
 };
 
 export default Profile;
