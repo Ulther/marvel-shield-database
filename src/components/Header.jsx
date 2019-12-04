2376e4c2d098cdd484bce6f147390d391e46e5f0
DaveM880 <eldeividas@gmail.com>
2019-12-04 14:11:05 +0000

    Hail hydra to be continued

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import shieldLogo from "./shieldLogo.png"
+import hydraLogo from "./hydraLogo.png"
 
+let logo = shieldLogo;
+const hailHydra=()=>{
+if (logo===shieldLogo){
+  logo = hydraLogo
+}else{
+  logo = shieldLogo
+}
+  return logo
+}
 const Header = () => {
   return (
     <header>
-      <img className= "headerLogo" src={shieldLogo} alt="shield logo"  ></img>
+      <img className= "headerLogo" src={logo} onClick={hailHydra} alt="shield logo"  ></img>
       <h1>Super Secret S.H.I.E.L.D. Database</h1>
     </header>
   );
